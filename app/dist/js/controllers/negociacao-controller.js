9144b5fba584d203186f958dc8689f1da2d039b8
devHikary <wrkkarina@gmail.com>
2021-09-19 11:42:49 -0300

    Evitando importar negociações duplicadas

--- app/dist/js/controllers/negociacao-controller.js
+++ app/dist/js/controllers/negociacao-controller.js
@@ -35,7 +35,15 @@ export class NegociacaoController {
         this.atualizaView();
     }
     importaDados() {
-        this.negociacoesService.obterNegociacoesDoDia()
+        this.negociacoesService
+            .obterNegociacoesDoDia()
+            .then(negociacoesDeHoje => {
+            return negociacoesDeHoje.filter(negociacoesDeHoje => {
+                return !this.negociacoes
+                    .lista()
+                    .some(negociacao => negociacao.ehIgual(negociacoesDeHoje));
+            });
+        })
             .then(negociacoesDeHoje => {
             for (let negociacao of negociacoesDeHoje) {
                 this.negociacoes.adiciona(negociacao);
