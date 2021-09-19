9fcb5191f3f4bdd71541f3987dfd8f2fff9fe61b
devHikary <wrkkarina@gmail.com>
2021-09-19 09:32:09 -0300

    Consumindo API

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -48,8 +48,25 @@ export class NegociacaoController {
     this.atualizaView();
   }
 
-  importaDados(): void{
-    alert('oi');
+  public importaDados(): void{
+    fetch('http://localhost:8080/dados')
+      .then(res =>{
+        return res.json();
+      })
+      .then((dados: any[]) => {
+        return dados.map(dadoDeHoje => {
+          return new Negociacao(
+            new Date(), 
+            dadoDeHoje.vezes, 
+            dadoDeHoje.montante)
+        })
+      })
+      .then(negociacoesDeHoje =>{
+        for(let negociacao of negociacoesDeHoje){
+          this.negociacoes.adiciona(negociacao)
+        }
+        this.negociacoesView.update(this.negociacoes);
+      })
   }
 
   private ehDiaUtil(data: Date) {
